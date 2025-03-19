3e56d7651e2e98e53a2e7fa5b5c656d105b81fcc
Arthur Lazarenko <Arthur.Lazarenko@icloud.com>
2025-03-19 22:40:02 +0200

    chore

--- Optimize Windows OS.cmd
+++ Optimize Windows OS.cmd
@@ -1,7 +1,12 @@
-::🧹 REMOVE XBOX GAMING OVERLAY
-powershell -Command "Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage -AllUsers"
-powershell -Command "reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR /f /t REG_DWORD /v "AppCaptureEnabled" /d 0"
-powershell -Command "reg add HKEY_CURRENT_USER\System\GameConfigStore /f /t REG_DWORD /v "GameDVR_Enabled" /d 0"
+::🧹 Disable Virtualization-based Security (VBS)
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v "EnableVirtualizationBasedSecurity" /t REG_DWORD /d 0 /f
 
-::🔙 MICROSOFT STORE: https://apps.microsoft.com/detail/9nzkpstsnw4p
+::🧹 Disable Hypervisor-protected Code Integrity (HVCI)
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "Enabled" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Security Health\State" /v "Hardware_HVCI_Off" /t REG_DWORD /d 0 /f
+
+::⏸️ Pause
 pause
+
+::🔗 Open documentation
+explorer "https://support.microsoft.com/en-us/windows/options-to-optimize-gaming-performance-in-windows-11-a255f612-2949-4373-a566-ff6f3f474613"
