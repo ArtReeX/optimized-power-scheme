d95400cfe79ccc45ad468b63e160dd05ea59fda7
Arthur Lazarenko <Arthur.Lazarenko@icloud.com>
2025-03-22 22:18:00 +0200

    chore

--- Optimize Windows OS.cmd
+++ Optimize Windows OS.cmd
@@ -1,12 +1,8 @@
-::🧹 Disable Virtualization-based Security (VBS)
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v "EnableVirtualizationBasedSecurity" /t REG_DWORD /d 0 /f
-
-::🧹 Disable Hypervisor-protected Code Integrity (HVCI)
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "Enabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows Security Health\State" /v "Hardware_HVCI_Off" /t REG_DWORD /d 0 /f
+::⚙️ Change Win32PrioritySeparation
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d 42 /f
 
 ::⏸️ Pause
 pause
 
 ::🔗 Open documentation
-explorer "https://support.microsoft.com/en-us/windows/options-to-optimize-gaming-performance-in-windows-11-a255f612-2949-4373-a566-ff6f3f474613"
+explorer "https://www.xbitlabs.com/win32priorityseparation-performance/"
